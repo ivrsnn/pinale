5491656250f04998b5093cadc4e3365d17a1636d
iversioncastro <iversoncastro0115@gmail.com>
2024-08-26 02:05:11 +0800

    first commit

---
+++ admin_page.php
@@ -0,0 +1,147 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if (!isset($admin_id)) {
+   header('location:admin_all_orders.php');
+}
+;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom admin css file link  -->
+   <link rel="stylesheet" href="./css/admin_style.css" />
+
+   <style>
+      body {
+         background-image: url('images/background_no-logo.png');
+         background-size: cover;
+         background-repeat: no-repeat;
+      }
+
+      .holder {
+         display: grid;
+         grid-template-columns: auto auto auto;
+         row-gap: 10px;
+         column-gap: 10px;
+      }
+   </style>
+</head>
+
+<body>
+
+   <?php @include 'admin_header.php'; ?>
+
+   <section class="dashboard">
+
+      <h1 class="title">dashboard</h1>
+
+      <div class="box-container">
+
+         <div class="holder">
+            <div class="box">
+               <?php
+               $total_pendings = 0;
+               $select_pendings = mysqli_query($conn, "SELECT * FROM `orders` WHERE payment_status = 'pending'") or die('query failed');
+               while ($fetch_pendings = mysqli_fetch_assoc($select_pendings)) {
+                  $total_pendings += $fetch_pendings['total_price'];
+               }
+               ;
+               ?>
+               <h3>PHP <?php echo $total_pendings; ?></h3>
+
+               <a href="pending-orders.php" class="btn">Total Pendings</a>
+
+
+            </div>
+
+            <div class="box">
+               <?php
+               $total_completes = 0;
+               $select_completes = mysqli_query($conn, "SELECT * FROM `orders` WHERE payment_status = 'completed'") or die('query failed');
+               while ($fetch_completes = mysqli_fetch_assoc($select_completes)) {
+                  $total_completes += $fetch_completes['total_price'];
+               }
+               ;
+               ?>
+               <h3>₱<?php echo $total_completes; ?></h3>
+
+               <a href="completed-orders.php" class="btn">Completed Payments</a>
+            </div>
+
+
+            <div class="box">
+               <?php
+               $select_orders = mysqli_query($conn, "SELECT * FROM `orders`") or die('query failed');
+               $number_of_orders = mysqli_num_rows($select_orders);
+               ?>
+               <h3><?php echo $number_of_orders; ?></h3>
+               <a href="admin_orders.php" class="btn">All Orders</a>
+
+            </div>
+
+            <div class="box">
+               <?php
+               $select_products = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+               $number_of_products = mysqli_num_rows($select_products);
+               ?>
+               <h3><?php echo $number_of_products; ?></h3>
+               <a href="admin_products.php" class="btn">products</a>
+            </div>
+
+            <div class="box">
+               <?php
+               $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE user_type = 'user'") or die('query failed');
+               $number_of_users = mysqli_num_rows($select_users);
+               ?>
+               <h3><?php echo $number_of_users; ?></h3>
+               <a href="admin_users.php" class="btn">View Users</a>
+            </div>
+
+            <div class="box">
+               <?php
+               $select_messages = mysqli_query($conn, "SELECT * FROM `message`") or die('query failed');
+               $number_of_messages = mysqli_num_rows($select_messages);
+               ?>
+               <h3><?php echo $number_of_messages; ?></h3>
+               <a href="admin_contacts.php" class="btn">View messages</a>
+            </div>
+         </div>
+
+      </div>
+
+   </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <script src="js/admin_script.js"></script>
+
+</body>
+
+</html>
---
+++ forgot.php
@@ -0,0 +1,130 @@
+<?php
+@include 'config.php';
+if (isset($_POST['submit'])) {
+   $filter_email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+   $email = mysqli_real_escape_string($conn, $filter_email);
+   $filter_pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
+   $pass = mysqli_real_escape_string($conn, md5($filter_pass));
+   $filter_cpass = filter_var($_POST['cpass'], FILTER_SANITIZE_STRING);
+   $cpass = mysqli_real_escape_string($conn, md5($filter_cpass));
+
+   $select_users = mysqli_query($conn, "SELECT * FROM `users` WHERE email = '$email'") or die('query failed');
+
+   if (mysqli_num_rows($select_users) > 0) {
+      if ($pass != $cpass) {
+         $message[] = 'confirm password not matched!';
+      }
+   } else {
+      $message[] = 'user not exist!';
+   }
+
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>FORGOT PASSWORD</title>
+
+   <link rel="stylesheet" href="./bootstrap-5.3.3-dist/css/bootstrap.min.css">
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="./css/style.css" />
+
+   <style>
+      body {
+         background-image: url('images/bgbgbg.jpg');
+         background-size: cover;
+         background-repeat: no-repeat;
+      }
+
+      #popup {
+         display: none;
+         align-items: center;
+         position: absolute;
+         top: 0;
+         bottom: 0;
+         left: 0;
+         right: 0;
+         background-color: rgba(153, 170, 187, 0.8);
+         z-index: 999;
+
+      }
+
+      .message-box {
+         display: flex;
+         justify-content: center;
+         width: 250px;
+         height: 150px;
+         background-color: white;
+         border: 1px solid black;
+         border-radius: 5%;
+         margin: auto;
+         padding: 1rem;
+         flex-direction: column;
+         align-items: center;
+
+      }
+
+      .message-box span {
+         font-size: 1.5rem;
+
+      }
+   </style>
+
+
+</head>
+
+<body>
+   <div id="popup">
+      <div class="message-box">
+         <span>Password Successfully Changed</span>
+         <a class="btn" href="login.php">OK</a>
+      </div>
+   </div>
+
+   <?php
+   if (isset($message)) {
+      foreach ($message as $message) {
+         echo '
+      <div class="message">
+         <span>' . $message . '</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+      }
+   }
+   ?>
+
+   <section class="form-container">
+
+      <form action="" method="post">
+         <h3>CHANGE PASSWORD</h3>
+         <input type="email" name="email" class="box" placeholder="enter your email" required>
+         <input type="password" name="pass" class="box" placeholder="enter your new password" required>
+         <input type="password" name="cpass" class="box" placeholder="confirm your new password" required>
+         <input type="submit" class="btn" name="submit" value="submit">
+
+      </form>
+
+   </section>
+
+</body>
+
+</html>
+
+<?php
+
+if (isset($_POST['submit'])) {
+   if ($pass == $cpass && empty($message)) {
+      mysqli_query($conn, "UPDATE users SET password = '$pass' WHERE email = '$email'") or die('query failed');
+      echo '<script>document.getElementById("popup").style.display="flex";</script>';
+   }
+}
+?>
